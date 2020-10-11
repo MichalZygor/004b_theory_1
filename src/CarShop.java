50148fef0ccd9ac2a9b3375f846f33a1424edcf5
Michal Zygor <michal@winami.com>
2020-10-12 00:15:47 +0200

    004b_theory: step 1

---
+++ src/CarShop.java
@@ -0,0 +1,7 @@
+public class CarShop {
+    public static void main(String[] args) {
+        Car car = new Car(2017, "Audi", "A5", "czarny");
+
+        System.out.println(car.model + " " + car.color);
+    }
+}
