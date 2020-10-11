9723f60ab54925d115298f3ef7cc2a0f7e850baf
Michal Zygor <michal@winami.com>
2020-10-12 00:28:22 +0200

    004b_theory: step 3 - this i wywyłanie jednego konstruktora w drugim

--- src/Car.java
+++ src/Car.java
@@ -5,18 +5,16 @@ public class Car {
     String color;
 
     Car(int y, String b, String m, String c){
-        year = y;
-        brand = b;
-        model = m;
+        this( y, b,  m); //musi być pierwsza instrukcją!!!
         color = c;
+//        System.out.println("Konstruktor 1");
     }
 
     Car(int y, String b, String m){
         year = y;
         brand = b;
         model = m;
+//        System.out.println("Konstruktor 2");
     }
 
-    Car(){
-    }
 }
